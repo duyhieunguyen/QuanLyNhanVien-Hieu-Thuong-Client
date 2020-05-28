29d32a87197ecd10bbb74cd1d7047d690369efcb
nguyenvanthuong0201 <55219637+nguyenvanthuong0201@users.noreply.github.com>
2020-05-28 15:56:14 +0700

    Client

--- WebClient/js/home.js
+++ WebClient/js/home.js
@@ -4,6 +4,7 @@ console.log(obj)
 if(obj)
 {
   if(obj.Role=="employee");
+  load_data();
 }
 else{
   document.location.href="../view/login.html"
@@ -21,10 +22,26 @@ $(document).ready(function(){
 document.getElementById("FullName_out").innerHTML=obj.FullName;
 document.getElementById("Email_out").innerHTML=obj.Email;
 ///xử lý đăng xuất 
-console.log(obj.FullName)
+console.log(obj)
 function dang_xuat(){
   localStorage.clear();
   document.location.href="../view/login.html"
+}
+
+//load Data cho Employee
+function load_data()
+{
+  var html="";
+  Doc_Danh_sach().forEach(employees => {
+    html +=`<tr>
+    <td scope="row">${employees.Account.UserName}</td>
+    <td>${employees.Account.Password}</td>
+    <td>${employees.Email}</td>
+    <td>${employees.Account.Role}</td>
+    <td>d</td>
+    </tr>`
+  });
+  myTable.innerHTML=html;
 }
 
 
--- WebClient/js/login.js
+++ WebClient/js/login.js
@@ -50,5 +50,44 @@ function xu_ly_login() {
     alert("Đăng nhập thất bại");
   }
 }
+var firebaseConfig = {
+  apiKey: "AIzaSyDt8336IDbpjOdqHPGwOuPTscXXCKKMcSA",
+  authDomain: "quanlynhanvien-136ee.firebaseapp.com",
+  databaseURL: "https://quanlynhanvien-136ee.firebaseio.com",
+  projectId: "quanlynhanvien-136ee",
+  storageBucket: "quanlynhanvien-136ee.appspot.com",
+  messagingSenderId: "413852244452",
+  appId: "1:413852244452:web:4c9ff415105ed92fe5cff1"
+}
+function login_google()
+{
+
+      // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var provider = new firebase.auth.GoogleAuthProvider();
+  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+  firebase.auth().languageCode = 'pt';
+  // To apply the default browser preference instead of explicitly setting it.
+  // firebase.auth().useDeviceLanguage();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+};
+
+
 
 
