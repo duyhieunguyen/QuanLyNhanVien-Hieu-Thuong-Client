897a1e7ccb13226f9161a650d8600ca663f6fca7
nguyenvanthuong0201 <55219637+nguyenvanthuong0201@users.noreply.github.com>
2020-06-02 01:27:04 +0700

    Register

--- WebClient/js/register.js
+++ WebClient/js/register.js
@@ -1,3 +1,17 @@
+//xử lý ẩn hiển password
+const pass_field = document.querySelector(".pass-key");
+const showBtn = document.querySelector(".show");
+showBtn.addEventListener("click", function () {
+  if (pass_field.type === "password") {
+    pass_field.type = "text";
+    showBtn.textContent = "";
+    showBtn.style.color = "#3498db";
+  } else {
+    pass_field.type = "password";
+    showBtn.textContent = "";
+    showBtn.style.color = "#222";
+  }
+});
 
 var encryptedAES;
 var kq;
